b6e680091615a88990e3e7b7c52ab85f1f839327
Talita Oliveira <talitagiovanna2011@gmail.com>
2023-11-27 12:04:22 -0300

    Adicionando exceptions no Exercicio4

---
+++ src/main/java/main/java/br/edu/uniaeso/ArquivoExercicio4/Sistema.java
@@ -0,0 +1,27 @@
+package main.java.br.edu.uniaeso.ArquivoExercicio4;
+
+public class Sistema {
+    
+    private String[] valores = new String[50];
+
+    public String getValorByIndice(int indice) throws ValorIncorretoException{
+        if(indice >= 0 && indice < valores.length) {
+            return valores[indice];
+        }
+
+        throw new ValorIncorretoException("Valor incorreto para o indice.");
+    }
+
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+
+        try {
+
+            String valorByIndice = sistema.getValorByIndice(50);
+            System.out.println(valorByIndice);
+            
+        } catch (ValorIncorretoException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/main/java/br/edu/uniaeso/ArquivoExercicio4/ValorIncorretoException.java
@@ -0,0 +1,7 @@
+package main.java.br.edu.uniaeso.ArquivoExercicio4;
+
+public class ValorIncorretoException extends Exception {
+    public ValorIncorretoException(String message) {
+        super(message);
+    }
+}
